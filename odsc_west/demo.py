f712deaebe8315b9e11e80c8b56277c7f4c121aa
Chase DeHan <chase.dehan@progleasing.com>
2017-11-04 10:57:45 -0600

    Added ODSC West presentation docs

---
+++ odsc_west/demo.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from boostaroota import BoostARoota
+import urllib
+
+#################
+#Madelon Dataset
+train_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/madelon/MADELON/madelon_train.data'
+# download the file
+raw_data = urllib.request.urlopen(train_url)
+train = pd.read_csv(raw_data, delim_whitespace=True, header=None)
+train.columns = ["Var"+str(x) for x in range(len(train.columns))]
+labels_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/madelon/MADELON/madelon_train.labels'
+raw_data = urllib.request.urlopen(labels_url)
+labels = pd.read_csv(raw_data, delimiter=",", header=None)
+labels.columns = ["Y"]
+
+
+########################################################################################################################
+#
+#  Test that BoostARoota is working
+#
+########################################################################################################################
+br = BoostARoota(metric='logloss')
+
+br.fit(train,labels)
+len(train.columns)
+len(br.keep_vars_)
+new_train = br.transform(train)
+new_train2 = br.fit_transform(train,labels)
+
+
+#Dimension Reduction
+print("Original training set has " + str(train.shape) + " dimensions. \n" +\
+"BoostARoota with .fit() and .transform() reduces to " + str(new_train.shape) + " dimensions. \n" +\
+"BoostARoota with .fit_transform() reduces to " + str(new_train2.shape) + " dimensions.\n" +\
+"The two methods may give a slightly different dimensions because of random variation as it is being refit")
+
